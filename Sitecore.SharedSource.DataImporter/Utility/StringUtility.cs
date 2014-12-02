22d47f36dd2f40cf28108e5e44159692cc50409d
mawiseman <mark@wiseman.net.au>
2014-12-02 14:44:29 +1100

    added sitecores ItemUtil.ProposeValidItemName to get valid item names

--- Sitecore.SharedSource.DataImporter/Utility/StringUtility.cs
+++ Sitecore.SharedSource.DataImporter/Utility/StringUtility.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using Sitecore.Data.Items;
 
 namespace Sitecore.SharedSource.DataImporter.Utility
 {
@@ -52,7 +53,9 @@ namespace Sitecore.SharedSource.DataImporter.Utility
         /// </summary>
         public static string GetNewItemName(string nameValue, int maxLength)
         {
-            return TrimText(StripInvalidChars(nameValue), maxLength, string.Empty);
+            string newItemName = StripInvalidChars(ItemUtil.ProposeValidItemName(nameValue));
+
+            return TrimText(newItemName, maxLength, string.Empty);
         }
 
         public static string TrimText(string val, int maxLength, string endingString)
@@ -60,16 +63,16 @@ namespace Sitecore.SharedSource.DataImporter.Utility
             string strRetVal = val;
             return (val.Length > maxLength) ? val.Substring(0, maxLength) + endingString : strRetVal;
         }
-
         public static string StripInvalidChars(string val)
         {
-			StringBuilder sb = new StringBuilder();
-			Dictionary<char,char> invalid = Sitecore.Configuration.Settings.InvalidItemNameChars.ToDictionary<char, char>(a => a);
-			foreach(char c in val){
-				if(!invalid.ContainsKey(c))
-					sb.Append(c);
-			}
-			
+            StringBuilder sb = new StringBuilder();
+            Dictionary<char, char> invalid = Sitecore.Configuration.Settings.InvalidItemNameChars.ToDictionary<char, char>(a => a);
+            foreach (char c in val)
+            {
+                if (!invalid.ContainsKey(c))
+                    sb.Append(c);
+            }
+
             return sb.ToString().Trim();
         }
     }
