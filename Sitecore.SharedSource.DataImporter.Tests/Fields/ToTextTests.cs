cc8224f30cc82be414a9511229d6234e56ccd847
Mark Stiles <mark.stiles@velir.com>
2016-02-04 17:00:35 -0500

    *working test

---
+++ Sitecore.SharedSource.DataImporter.Tests/Fields/ToTextTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Sitecore.Data.Items;
+using Sitecore.FakeDb;
+using Sitecore.FakeDb.Serialization;
+using Sitecore.SharedSource.DataImporter.Extensions;
+using Sitecore.SharedSource.DataImporter.Mappings.Fields;
+using Sitecore.SharedSource.DataImporter.Providers;
+
+namespace Sitecore.SharedSource.DataImporter.Tests.Fields
+{
+    [TestFixture, Category("ToText Tests")]
+    public class ToTextTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+ 
+        }
+        
+        [Test]
+        public void HowToDeserializeItem() {
+            using (Db db = new Db {
+                new DsDbItem("/sitecore/content/home")
+            })
+            {
+                var home = db.GetItem("/sitecore/content/home");
+                Assert.IsNotNull(home);
+            }
+        }
+    }
+}
